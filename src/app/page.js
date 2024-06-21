4aec483a13116acb8096ea9509db2f69b8565a25
Brian Emilius <be@rts.dk>
2024-06-21 12:14:31 +0200

    refactor: Remove error handling from Home component
    The code changes in this commit remove the error handling logic from the Home component. The useAxios hook now only returns the loading and data properties, without the error property. This simplifies the code and improves reusability.

--- src/app/page.js
+++ src/app/page.js
@@ -2,13 +2,12 @@
 import useAxios from "@/hooks/use-axios"
 
 export default function Home() {
-	const { loading, data, error } = useAxios("/api/test")
+	const { loading, data } = useAxios("/api/test")
 
 	return (
 		<>
 			{loading && <p>Loading...</p>}
 			{data && <h1>{data.message}</h1>}
-			{error && <p className="text-red-600">{error.message}</p>}
 		</>
 	)
 }
