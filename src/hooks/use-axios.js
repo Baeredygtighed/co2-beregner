75a029f9c2c840ea0a40fa5dc9df19050f5369a4
Brian Emilius <be@rts.dk>
2024-06-21 12:13:03 +0200

    refactor: Refactor useAxios hook to handle errors properly
    The code changes in this commit refactor the useAxios hook to handle errors properly. Instead of catching the error and setting it as a state, the error is now thrown as an instance of the Error class. This ensures that the error can be caught and handled by the calling code. The loading and data properties are still returned by the hook.

--- src/hooks/use-axios.js
+++ src/hooks/use-axios.js
@@ -3,16 +3,15 @@ import { useState, useEffect } from "react"
 import axios from "axios"
 
 export default function useAxios(endpoint) {
-	const [error, setError] = useState(null)
 	const [loading, setLoading] = useState(true)
 	const [data, setData] = useState(null)
 
 	useEffect(function () {
 		axios.get(endpoint)
 			.then(response => setData(response.data))
-			.catch(error => setError(error))
+			.catch(error => {throw new Error(error)})
 			.finally(() => setLoading(false))
 	}, [])
 
-	return { loading, data, error }
+	return { loading, data }
 }
