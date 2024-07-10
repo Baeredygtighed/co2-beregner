aeca6854bc9f4bc61e0815eae1a74d292e743869
Henrik Andersen <han@rts.dk>
2024-07-10 18:57:32 +0200

    feat onChange event handler

--- src/components/add-check-box.js
+++ src/components/add-check-box.js
@@ -1,26 +1,26 @@
 "use client";
 
-import { useRef } from "react";
 import { RxPlus } from "react-icons/rx";
 import { RxCheck } from "react-icons/rx";
 import { twMerge } from "tailwind-merge";
 
-export default function AddCheckBox({ className }) {
-
-    const checkbox = useRef(null);
+export default function AddCheckBox({ className, onChange = null, checked = false}) {
 
     const clickHandler = event => {
-        event.target.dataset.checked = event.target.dataset.checked === "true" ? "false" : "true";
+
+        checked = event.target.dataset.checked === "true" ? "false" : "true";
+
+        if (onChange) onChange(new CustomEvent("change", {detail: { checked: checked === "true" }}));
     }
 
     return (
-        <button onClick={clickHandler} data-checked="false" ref={checkbox} className={twMerge(`
+        <div onClick={clickHandler} data-checked={checked} className={twMerge(`
             border-2 border-black size-7 flex justify-center items-center rounded-md text-black transition-all
             [&>.plus-icon]:block [&>.check-icon]:hidden
             [&>.plus-icon]:data-[checked=true]:hidden [&>.check-icon]:data-[checked=true]:block data-[checked=true]:text-blue-500 data-[checked=true]:border-blue-500
         `, className)}>
             <RxPlus className="plus-icon size-6 pointer-events-none" />
             <RxCheck className="check-icon size-6 pointer-events-none" />
-        </button>
+        </div>
     );
 }
