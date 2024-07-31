4edcc7bcb8edf3b73a51f5c55b139b265a6a5ddc
Henrik Andersen <han@rts.dk>
2024-07-31 14:18:04 +0200

    feat: Add new material icons for different categories

--- src/components/materials/material-icon.js
+++ src/components/materials/material-icon.js
@@ -1,11 +1,13 @@
-import { GiWoodBeam } from "react-icons/gi";
-import { GiConcreteBag } from "react-icons/gi";
+import { GiWoodBeam, GiConcreteBag, GiRolledCloth, GiBrickPile, GiPapers } from "react-icons/gi";
 import { GrStatusUnknown } from "react-icons/gr";
 
 export default function MaterialIcon({ name, className }) {
     const categoryIcons = [
         { name: 'træ', icon: GiWoodBeam },
         { name: 'beton', icon: GiConcreteBag },
+        { name: 'beklædning', icon: GiRolledCloth },
+        { name: 'sten', icon: GiBrickPile },
+        { name: 'isolering', icon: GiPapers  },
     ];
 
     const Icon = categoryIcons.find(icon => icon.name === name.toLowerCase())?.icon ?? GrStatusUnknown;
