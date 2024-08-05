54ab1b8384e730f126a406081e7a54fd5541c08f
Henrik Andersen <han@rts.dk>
2024-08-05 10:47:12 +0200

    chore: Update DashboardPage layout and components
    This commit updates the layout and components of the DashboardPage. The changes involve adding buttons for navigating to the materials and terms pages, improving the visual appearance and user experience of the page.

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -1,5 +1,14 @@
 "use client"
 
+import { Link, Button } from "@nextui-org/react"
+
 export default function DashboardPage() {
-	return <h1>Dashboard</h1>
+	return (
+		<div className="flex flex-col p-4 gap-4">
+			<h1 className="text-center text-3xl font-semibold">Dashboard</h1>
+			<Button as={Link} href="/dashboard/materials" color="primary" size="lg">Tilføj/ret materialer</Button>
+			<Button as={Link} href="/dashboard/terms" color="primary" size="lg">Tilføj/ret ord</Button>
+		</div>
+
+	)
 }
