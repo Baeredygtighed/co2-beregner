32d0d8008efde86cefb115c4ab6167280e0ec219
Brian Emilius <be@rts.dk>
2024-08-05 14:04:50 +0200

    chore: Update middleware.js to handle production environment for axios post request
    This commit updates the middleware.js file to handle the production environment for the axios post request. It modifies the URL used for the post request to include the correct endpoint based on the environment. This change ensures that the post request is made to the correct API endpoint in the production environment, improving the functionality and reliability of the application.

--- src/middleware.js
+++ src/middleware.js
@@ -8,7 +8,7 @@ export async function middleware(request) {
 			return Response.redirect(new URL("/login", request.url))
 		}
 	
-		const response = await axios.post(request.nextUrl.origin + "/api/decryptedsessioncookie", {user})
+		const response = await axios.post(process.env.production ? "https://co2-beregner.onrender.com/api/decryptedsessioncookie" : request.nextUrl.origin + "/api/decryptedsessioncookie", {user})
 	
 		if (response.data.user !== "admin") {
 			return Response.redirect(new URL("/login", request.url))
